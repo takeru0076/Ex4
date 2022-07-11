0b8a6af45d3e6d4d60edb6b10f51a992c5865786
takeru0076 <takeru200276@icloud.com>
2022-07-11 14:31:39 +0900

    User name added

--- Ex4.java
+++ Ex4.java
@@ -1,9 +1,15 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Ex4 {
     public static void main(String[] args) {
         int head = 0;
         int tail = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Who are you?");
+        System.out.print(">");
+        String name = scanner.next();
+        System.out.println("Hello," + name);
         System.out.println("Tossing a coin...");
         for (int i = 0; i < 3; i++) {
             Random rnd = new Random();
